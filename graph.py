3775fc21727c03f0039fe0bf7408e995fa7af9db
s73207 <rafaelaneff@posteo.de>
2019-11-26 05:17:51 +0100

    attr saved with ids

--- graph.py
+++ graph.py
@@ -5,6 +5,8 @@ from bs4 import BeautifulSoup
 
 
 path = "../cyberlandscape/data.graphml"
+n_names = []
+e_names = []
 
 with open(path) as file:
     soup = BeautifulSoup(file, "lxml")
@@ -13,14 +15,21 @@ with open(path) as file:
     edges = soup.findAll("key",{"for":"edge"})
     print("edges ", edges)
 
-#print " --- edge attr names --- "
+#TODO: Namen mit id speichern
+#print " --- node attr names --- "
+for node in nodes:
+    #print (node['id'])
+    if node.has_attr('attr.name'):
+    	n_names.append({'id': node['id'], 'attr':node['attr.name']})
+
 for edge in edges:
     print (edge['id'])
     if edge.has_attr('attr.name'):
-    	print (edge['attr.name'])
+    	e_names.append({'id': edge['id'], 'attr':edge['attr.name']})
 
-G =nx.read_graphml(path)
+print (n_names, e_names)
+G = nx.read_graphml(path)
 #nx.draw(G)
 #plt.show()
 #print(G.nodes(), G.edges)
-print(G.number_of_nodes(), G.number_of_edges())
+#print(G.number_of_nodes(), G.number_of_edges())
